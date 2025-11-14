8dd8a0fc846fd0e46cb3047bde8efebb05739e5a
AdithyaAlokkan <adithya.alokkan@gmail.com>
2025-11-14 14:52:29 +0000

    cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,8 @@
+import NavBar from "./components/NavBar/NavBar"
+
 function App() {
   return (
-    <>
-      <div className='card'>
-        <h1>Lorem ipsum dolor sit amet</h1>
-        <h2>Consectetur adipiscing elit</h2>
-        <h3>Vivamus sit amet orci a purus consequat tincidunt</h3>
-        <div>
-          Suspendisse id semper odio. Proin semper lorem sem, eu tincidunt felis
-          euismod at. Donec gravida, metus nec volutpat pharetra, magna arcu eleifend
-          mi, eget aliquet eros velit nec magna. Suspendisse sit amet lobortis dui.
-        </div>
-      </div>
-    </>
+    <NavBar/>
   )
 }
 
